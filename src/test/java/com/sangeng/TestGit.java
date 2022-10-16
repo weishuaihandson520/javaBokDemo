6091afabfbbd20597fa913d0fac15c3cef57c328
hong-caixia <1362941253@qq.com>
2022-10-16 19:25:36 +0800

    新的修正的项目

--- src/test/java/com/sangeng/TestGit.java
+++ src/test/java/com/sangeng/TestGit.java
@@ -2,7 +2,8 @@ package com.sangeng;
 
 public class TestGit {
     public static void main(String[] args) {
+        System.out.println("master");
         System.out.println("hot-fix");
-//        System.out.println();
+        System.out.println("hot-fix1");
     }
 }
